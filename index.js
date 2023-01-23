22a832cfd10bc81b2fdc5b103cd76f4db7a70b5e
Makayla Koholua <112517809+mkoholua@users.noreply.github.com>
2023-01-23 12:16:00 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+//player 1 dice//
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+//player 2 dice//
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//winner//
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+}
+
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
